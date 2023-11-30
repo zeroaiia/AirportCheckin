dd7cc96fe6903305d56d46331ab42ad9ef33d607
Aiia <458007508@qq.com>
2023-11-30 14:56:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,11 @@ data = {
 }
 try:
     print('进行登录...')
+    print(login_url)
+    print(email)
+    print(passwd)    
     response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
+    print(response)
     # 进行签到
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
